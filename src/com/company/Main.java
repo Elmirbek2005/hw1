d5a04019f3c7272830925a827cb60d38b60da2c0
Elmirbek2005 <elmirbek2005.kg@gmail.com>
2022-03-05 19:47:26 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+     String MLO;
+     final int NUM =0;
+     String world = "githab";
+     MLO=NUM+world;
+        System.out.println(MLO);
+        if (NUM < 0){
+            System.out.println("Вы сохранили отрицательное число");
+        }else  if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число число");
+        }else {
+            System.out.println("Вы сохранили ноль");
+        }
+
+    }
+}
